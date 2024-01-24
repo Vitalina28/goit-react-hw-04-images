be8b88a3bb7d2fef7d036356bc34edff411ddf40
Vitalina28 <132218868+Vitalina28@users.noreply.github.com>
2024-01-24 23:23:47 +0100

    update App and Modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 
 import axios from 'axios';
 import toast, { Toaster } from 'react-hot-toast';
@@ -11,76 +11,67 @@ import { Button } from './Button/Button';
 
 const PER_PAGE = 12;
 
-export default class App extends Component {
-  state = {
-    searchQuery: '',
-    images: [],
-    page: 1,
-    isLoading: false,
-    selectedImage: '',
+export default function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
+
+  const handleFormSubmit = async searchQuery => {
+    setSearchQuery(searchQuery);
+    setImages([]);
+    setPage(1);
   };
 
-  handleFormSubmit = async searchQuery => {
-    this.setState({ searchQuery, images: [], page: 1 });
-  };
-
-  async componentDidUpdate(_, prevState) {
+  useEffect(() => {
     const BASE_URL = 'https://pixabay.com/api/';
     const API_KEY = '40708369-d6dc8a1f6c8c52c3bc15ef748';
 
-    if (
-      prevState.searchQuery !== this.state.searchQuery ||
-      prevState.page !== this.state.page
-    ) {
-      this.setState({ isLoading: true });
+    if (searchQuery === '') {
+      return;
+    }
+
+    async function fetchData() {
+      setIsLoading(true);
       try {
         const response = await axios.get(
-          `${BASE_URL}?key=${API_KEY}&q=${this.state.searchQuery}&orientation=horizontal&per_page=${PER_PAGE}&page=${this.state.page}`
+          `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&orientation=horizontal&per_page=${PER_PAGE}&page=${page}`
         );
-        this.setState(prevState => ({
-          images: [...prevState.images, ...response.data.hits],
-        }));
+        setImages(prevImages => [...prevImages, ...response.data.hits]);
       } catch (error) {
         toast.error('Reload the page and try again');
       } finally {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       }
     }
-  }
+    fetchData();
+  }, [searchQuery, page]);
 
-  handleChangePage = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const handleChangePage = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  handleOpenModal = largeImageURL => {
-    this.setState({ selectedImage: largeImageURL });
+  const handleOpenModal = largeImageURL => {
+    setSelectedImage(largeImageURL);
   };
 
-  handleCloseModal = () => {
-    this.setState({ selectedImage: '' });
+  const handleCloseModal = () => {
+    setSelectedImage('');
   };
 
-  render() {
-    const { images, isLoading, selectedImage } = this.state;
-
-    return (
-      <>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        {images.length > 0 && (
-          <ImageGallery images={images} onClick={this.handleOpenModal} />
-        )}
-        {isLoading && <Loader />}
-        {images.length > 0 && !isLoading && (
-          <Button onClick={this.handleChangePage} />
-        )}
-        <Toaster />
-        {selectedImage && (
-          <Modal
-            onClose={this.handleCloseModal}
-            selectedImage={selectedImage}
-          />
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <Searchbar onSubmit={handleFormSubmit} />
+      {images.length > 0 && (
+        <ImageGallery images={images} onClick={handleOpenModal} />
+      )}
+      {isLoading && <Loader />}
+      {images.length > 0 && !isLoading && <Button onClick={handleChangePage} />}
+      <Toaster />
+      {selectedImage && (
+        <Modal onClose={handleCloseModal} selectedImage={selectedImage} />
+      )}
+    </>
+  );
 }
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,35 +1,29 @@
 import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
-import { Component } from 'react';
+import { useEffect } from 'react';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export const Modal = ({ onClose, selectedImage }) => {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
 
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    return createPortal(
-      <div className={css.Overlay} onClick={this.props.onClose}>
-        <div className={css.Modal}>
-          <img
-            src={this.props.selectedImage.largeImageURL}
-            alt={this.props.selectedImage.tags}
-          />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <div className={css.Overlay} onClick={onClose}>
+      <div className={css.Modal}>
+        <img src={selectedImage.largeImageURL} alt={selectedImage.tags} />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
